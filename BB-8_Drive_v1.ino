d6a45718bddc42d6f1c7bdc464e7a7c7c4027c98
Alaina Hardie <alaina@precisionnutrition.com>
2016-01-07 20:39:10 -0500

    Fix MPU6050
    Update setup() routine - just steal from DMP example.

--- BB-8_Drive_v1.ino
+++ BB-8_Drive_v1.ino
@@ -88,29 +88,58 @@ char navCommand; // Variable that holds the navigation command read from Bluetoo
 
 /* =================================================================
  * setupMPU() - Configure the mpu for DMP mode
+ *
+ * Copied from https://github.com/jrowberg/i2cdevlib/blob/master/Arduino/MPU6050/Examples/MPU6050_DMP6/MPU6050_DMP6.ino
  */
 void setupMPU() {
   // Set up I2C bus for mpu
   Wire.begin();
-  Wire.setClock(31000L);
+  Wire.setClock(31000L); // Slow down the refresh rate to allow it to do other robot-y things like move
 
-  // Set up mpu
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
 
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
   mpu.setXGyroOffset(220);
   mpu.setYGyroOffset(76);
   mpu.setZGyroOffset(-85);
   mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
 
-  // Turn on the DMP, now that it's ready
-  mpu.setDMPEnabled(true);
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
 
-  // enable Arduino interrupt detection
-  attachInterrupt(0, dmpDataReady, RISING);
-  mpuIntStatus = mpu.getIntStatus();
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
 
-  // set our DMP Ready flag so the main loop() function knows it's okay to use it
-  dmpReady = true;
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
 }
 
 /* =================================================================
